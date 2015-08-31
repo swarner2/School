cc03edb88ff37580a647fae2528c66dc14acc20b
ThornWinters <MichaelJWarner@hotmail.com>
2015-08-31 15:51:13 -0400

    got rendering and slicing working

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is meant to be simple in structure and technology so there is no need for a
 There is no SPA as this will create a higher barrer to editing and creating new content.
 
 This will have a simple node server that servers up directories from the app folder. Inside the app folder there are folder dividing the different iframes the teacher is needing.
+
+The key to this project is the KISS rule with both sturcture as well as well as the technology being used.
+
+The Server itself is just a static file server that gives off anything in the client folder.
--- client/sentenceSlicing/js/app.js
+++ client/sentenceSlicing/js/app.js
@@ -2,30 +2,82 @@
 
 
 
-var sentence = 'I am a sentence slice me. No, for real slice me up. I am here to be sliced! Do you know what I mean?';
+var text = 'I am a sentence slice me. No, for real slice me up. I am here to be sliced! Do you know what I mean?';
 
 $(document).ready(function(){
-	console.log('hit')
-	placeSentenceInID(sentence, 'sentence');
+	//createSentence(sentence, '#sentence');
+	createText('#sentence', text);
 });
 
-var placeSentenceInID = function(sentence, destinationElement){
-	var html = '<span class="sentence">';
-	_.each(sentence.split(' '), function(element){
-		html += '<span class="word">'+ element +'</span>';
-	})
-	html += '</span>';
-	$("#" + destinationElement).html(html);
-}
+var createText = function(renderPoint, text){
+	
+	var getSentences = function(text){
+		//This will find all the . ? and ! and show the index of their locations
+		var getIndex = function(text){
+			var character = ['.','?','!'];
+			var index = -1;
+			_.each(character, function(element){
+				var indexOf = text.indexOf(element);
+				if(index > indexOf && indexOf !== -1 || index === -1) index = indexOf;
+			});
+			if(index === -1) index = text.length;
+			return index;
+		};
+		//This will fill the array with the sliced up parts
+		var array = [];
+		while(text.length){
+			var index = getIndex(text)+1;
+			array.push(text.slice(0,index));
+			text = text.slice(index,text.length);
+		}
 
-
-//----not finished
-var textToSentences = function(text){
-	var periodIndex = sentence.indexOf('.');
-	var exclamationIndex = sentence.indexOf('!');
-	var questionIndex = sentence.indexOf('?');
-	//base case
-	if(periodIndex === -1 && exclamationIndex === -1 && questionIndex === -1){
-		return;
+		return array;
 	}
+	var render = function(sentences){
+		var renderSentences = function(sentences, renderPoint){
+			_.each(sentences, function(sentence){
+				var html = '<div class="sentence">';
+				_.each(sentence.split(' '), function(element){
+					html = html.concat(
+						'<span class="word">',
+							element,
+						'</span>'
+					);
+				});
+				html += '</div>';
+				$(renderPoint).html($(renderPoint).html() + html);
+				$( ".word" ).click(function() {
+				  console.log('hit');
+				});
+			});
+		}
+		var renderAnswers = function(answers, renderPoint){
+			var html = '';
+			_.each(answers, function(element){
+				html = html.concat(
+				'<div>',
+					'<span>',
+						element,
+					'</span>',
+					'<input>',
+					'<button id="removeSentence">X</button>',
+				'</div>'
+				);
+			});
+			$(renderPoint).html(html);
+		}
+
+		renderSentences(sentences, '.sentences');
+		renderAnswers(['adlsfkj','adsf','hi there', 'what do you what'],'.answers');
+	}
+
+	$(renderPoint).html(''.concat(
+		'<div class="paragraph">',
+			'<div class="sentences">',
+			'</div>',
+			'<div class="answers">',
+			'</div>',
+		'</div>'
+	));
+	render(getSentences(text));
 }
