0d819b0e0dcc8b540f1624c2623ac5c09886be67
ThornWinters <MichaelJWarner@hotmail.com>
2015-09-04 22:15:41 -0400

    added url slicer

--- client/sentenceSlicing/js/app.js
+++ client/sentenceSlicing/js/app.js
@@ -1,8 +1,29 @@
 
 
 
+var QueryString = function () {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
 
-var text = 'I am a sentence slice me; No, for real slice me up. I am here to be sliced! Do you know what I mean?';
+//console.log(QueryString.text);
+
+var text = QueryString.text;
+
+
 
 $(document).ready(function(){
 	//createSentence(sentence, '#sentence');
